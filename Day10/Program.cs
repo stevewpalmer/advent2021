b20be5e2c657b0d922a3136aa68032ac6935ad83
Steven Palmer <steve@stevewpalmer.uk>
2021-12-13 11:17:08 +0000

    Day 10 and 11

---
+++ Day10/Program.cs
@@ -0,0 +1,50 @@
+﻿string[] lines = File.ReadAllLines("day10.txt");
+
+int totalErrorScore = 0;
+List<long> autocompleteScores = new();
+
+char[] openingTag = { '(', '[', '{', '<' };
+char[] closingTag = { ')', ']', '}', '>' };
+int[] errorScore = { 3, 57, 1197, 25137 };
+
+foreach (string line in lines)
+{
+    Stack<char> chunkStack = new();
+
+    foreach (char ch in line.ToCharArray())
+    {
+        int indexOfTag = Array.IndexOf(openingTag, ch);
+        if (indexOfTag >= 0)
+        {
+            chunkStack.Push(closingTag[indexOfTag]);
+        }
+        else
+        {
+            indexOfTag = Array.IndexOf(closingTag, ch);
+            if (indexOfTag >= 0)
+            {
+                if (ch != chunkStack.Pop())
+                {
+                    totalErrorScore += errorScore[indexOfTag];
+                    chunkStack.Clear();
+                    break;
+                }
+            }
+        }
+    }
+
+    if (chunkStack.Count > 0)
+    {
+        long autocompleteScore = 0;
+        while (chunkStack.Count != 0)
+        {
+            autocompleteScore = autocompleteScore * 5 + (Array.IndexOf(closingTag, chunkStack.Pop()) + 1);
+        }
+        autocompleteScores.Add(autocompleteScore);
+    }
+}
+
+long totalAutocompleteScore = autocompleteScores.OrderBy(a => a).ElementAt(autocompleteScores.Count / 2);
+
+Console.WriteLine($"Puzzle 1 answer : {totalErrorScore}");
+Console.WriteLine($"Puzzle 2 answer : {totalAutocompleteScore}");
---
+++ Day11/Program.cs
@@ -0,0 +1,55 @@
+﻿int [][] map = File.ReadAllLines("day11.txt").Select(l => l.ToCharArray().Select(c => c - '0').ToArray()).ToArray();
+
+int totalFlashes = 0;
+
+void FlashOctopus(int x, int y) {
+    if (x >= 0 && y >= 0 && y < map.Length && x < map[y].Length)
+    {
+        ++map[y][x];
+        if (map[y][x] == 10)
+        {
+            FlashOctopus(x - 1, y - 1);
+            FlashOctopus(x, y - 1);
+            FlashOctopus(x + 1, y - 1);
+            FlashOctopus(x - 1, y);
+            FlashOctopus(x + 1, y);
+            FlashOctopus(x - 1, y + 1);
+            FlashOctopus(x, y + 1);
+            FlashOctopus(x + 1, y + 1);
+
+            totalFlashes++;
+        }
+    }
+}
+
+int firstSynchronizedFlashStep = -1;
+for (int steps = 1; steps <= 100 || firstSynchronizedFlashStep < 0; steps++)
+{
+    for (int y = 0; y < map.Length; y++)
+    {
+        for (int x = 0; x < map[y].Length; x++)
+        {
+            FlashOctopus(x, y);
+        }
+    }
+
+    int countOfFlashesInStep = 0;
+    for (int y = 0; y < map.Length; y++)
+    {
+        for (int x = 0; x < map[y].Length; x++)
+        {
+            if (map[y][x] > 9)
+            {
+                ++countOfFlashesInStep;
+                map[y][x] = 0;
+            }
+        }
+    }
+    if (countOfFlashesInStep == map.Length * map[0].Length && firstSynchronizedFlashStep < 0)
+    {
+        firstSynchronizedFlashStep = steps;
+    }
+}
+
+Console.WriteLine($"Puzzle 1 answer : {totalFlashes}");
+Console.WriteLine($"Puzzle 2 answer : {firstSynchronizedFlashStep}");
